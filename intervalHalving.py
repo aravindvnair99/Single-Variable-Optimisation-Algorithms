756f2dd159fbacbed34f9d09e87c12fc7266d005
Priya Potti <BLENU4CSE18091@bl.students.amrita.edu>
2020-10-04 10:31:00 +0530

    Modified file to eliminate insecure use of eval() Function
    Signed-off-by: Priya Potti <BLENU4CSE18091@bl.students.amrita.edu>

--- intervalHalving.py
+++ intervalHalving.py
@@ -1,19 +1,33 @@
 # Algorithm for Interval Halving Method
-print("Pls give termination parameter as zero to proceed with number of iterations")
+# An alternative of eval function
+from math import exp,sin,cos
+def eval_expr(input_string):
+     # Step 1
+     allowed_names = {"sum":sum, "sin":sin, "cos":cos, "exp": exp, "pow":pow, "x":x}
+     # Step 2
+     code = compile(input_string, "<string>", "eval")
+     # Step 3
+     for name in code.co_names:
+         if name not in allowed_names:
+             # Step 4
+             raise NameError(f"Use of {name} not allowed")
+     return eval(code, {"__builtins__": {}}, allowed_names)
+
+print("Please give termination parameter as zero to proceed with number of iterations.")
 a,b,e=map(float,input("Enter lower limit, higher limit and termination parameter: ").split())
-expr=input("Enter expression for x ")
-print("(Pls give no. of iterations as considerably a high number to check with termination parameter)")
+expr=input("Enter expression for x: ")
+print("Please give no. of iterations as considerably a high number to check with termination parameter.")
 i=int(input("Enter number of iterations to be performed: "))
-c=0
+c=0 # To maintain count of no. of iterations 
 L=abs(b-a)
 xm=(a+b)/2
 while(True):  
-    x=xm;fm=eval(expr)
+    x=xm;fm=eval_expr(expr)
     x1=a+L/4
-    x=x1;f1=eval(expr)
+    x=x1;f1=eval_expr(expr)
     if(f1>fm):
         x2=b-L/4
-        x=x2;f2=eval(expr)
+        x=x2;f2=eval_expr(expr)
         if(f2<fm):
             a=xm
             xm=x2
@@ -27,5 +41,4 @@ while(True):
     L=abs(b-a)
     if(L<=e or c>=i):
       break
- 
-print("Minimum lies in the interval ",a," and ",b)
+print("Minimum lies in the interval ",round(a,4)," and ",round(b,4))
