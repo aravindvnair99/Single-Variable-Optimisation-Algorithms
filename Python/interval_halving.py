4b29140d6c6dd515c00ffab554e8e4a9d01db73b
Aravind V. Nair <22199259+aravindvnair99@users.noreply.github.com>
2021-02-12 15:32:01 +0530

    Update interval_halving.py

--- Python/interval_halving.py
+++ Python/interval_halving.py
@@ -1,33 +1,20 @@
-# Algorithm for Interval Halving Method
-# An alternative of eval function
-from math import exp,sin,cos
-def eval_expr(input_string):
-     # Step 1
-     allowed_names = {"sum":sum, "sin":sin, "cos":cos, "exp": exp, "pow":pow, "x":x}
-     # Step 2
-     code = compile(input_string, "<string>", "eval")
-     # Step 3
-     for name in code.co_names:
-         if name not in allowed_names:
-             # Step 4
-             raise NameError(f"Use of {name} not allowed")
-     return eval(code, {"__builtins__": {}}, allowed_names)
-
-print("Please give termination parameter as zero to proceed with number of iterations.")
+#3 Interval Halving Method
+from math import exp, sin, cos, log
+print("Pls give termination parameter as zero to proceed with number of iterations")
 a,b,e=map(float,input("Enter lower limit, higher limit and termination parameter: ").split())
-expr=input("Enter expression for x: ")
-print("Please give no. of iterations as considerably a high number to check with termination parameter.")
+expr=input("Enter expression for x ")
+print("(Pls give no. of iterations as considerably a high number to check with termination parameter)")
 i=int(input("Enter number of iterations to be performed: "))
-c=0 # To maintain count of no. of iterations 
+c=0
 L=abs(b-a)
 xm=(a+b)/2
 while(True):  
-    x=xm;fm=eval_expr(expr)
+    x=xm;fm=eval(expr)
     x1=a+L/4
-    x=x1;f1=eval_expr(expr)
+    x=x1;f1=eval(expr)
     if(f1>fm):
         x2=b-L/4
-        x=x2;f2=eval_expr(expr)
+        x=x2;f2=eval(expr)
         if(f2<fm):
             a=xm
             xm=x2
@@ -41,4 +28,5 @@ while(True):
     L=abs(b-a)
     if(L<=e or c>=i):
       break
-print("Minimum lies in the interval ",round(a,4)," and ",round(b,4))
+ 
+print("Minimum lies in the interval ",round(a,5)," and ",round(b,5))
